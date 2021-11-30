9b421811cb181df9d43eeb7efa63d1bfdd300ff6
Aniket Agarwal <aniketagarwal57@gmail.com>
2021-11-30 20:06:51 +0530

    feat: add memberCount message handler

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -1,3 +1,4 @@
+
 import { MessageEmbed } from "discord.js";
 import { CONSTANTS } from "./constants";
 
@@ -8,6 +9,19 @@ export const createBasicEmbed = (
       .setColor("BLUE")
       .setTitle(msg.title)
       .setDescription(msg.message)
+      .setTimestamp()
+      .setFooter(CONSTANTS.FOOTER);
+  };
+
+  export const membersCountMessage = (membersCount: string, botCount: string) => {
+    return new MessageEmbed()
+      .setTitle(`It's ${+membersCount + +botCount}`)
+      .setColor("GREEN")
+      .setDescription(
+        `I dove deep into the server and counted the members for you! 🌊`
+      )
+      .addField('Current Members', membersCount, true)
+      .addField('Current Bots', botCount, true)
       .setTimestamp()
       .setFooter(CONSTANTS.FOOTER);
   };
