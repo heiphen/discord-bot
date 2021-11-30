638593f0ad2c1e564bd0002fbb3121f9f81e5f77
Aniket Agarwal <aniketagarwal57@gmail.com>
2021-11-30 20:05:55 +0530

    chore: add commands and update welcome message

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,12 +5,16 @@ export const INFO = {
     MEMBER_JOIN: (member: GuildMember) => {
       return {
         title: `A new member joined the server 🥳!`,
-        message: `<@${member.id}> joined the server! Welcome home!\nPlease check out ${member.guild.channels.cache.get('914591677551902740')} for more info.`,
+        message: `Welcome <@${member.id}> to the server!\nPlease check out ${member.guild.channels.cache.get('914591677551902740')} for more info.`,
       };
     },
 };
 
 export const CONSTANTS = {
-    FOOTER: "Powered by HEIPHEN",
+    FOOTER: `Heiphen`,
 };
 
+export const COMMANDS = {
+  prefix: "#bot",
+  membercount: "membercount",
+};
