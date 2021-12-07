d6cb0b7e409feb806b27602f4f97497365986467
Aniket Agarwal <aniketagarwal57@gmail.com>
2021-12-07 20:50:41 +0530

    chore: change code

--- src/helper/createRoleCode.ts
+++ src/helper/createRoleCode.ts
@@ -1,26 +1,41 @@
-import { Message } from "discord.js";
+import { Channel, Message, MessageEmbed, TextChannel } from "discord.js";
 import { Collection } from "mongodb";
 import { nanoid } from "nanoid";
+import { CONSTANTS } from "../utils/constants";
 
 
 export const handleCreateCode = async (incomingMessage: Message, userRoleCol: Collection) => {
     try {
-        const userTypedRole = incomingMessage.content.split(" ")[2];
+        const userTypedRole = incomingMessage.content.split(/\s+/)[2];
         const code = nanoid().substr(0,6);
     const newUser = await userRoleCol.findOne({id: incomingMessage.author.id});
     if (newUser) {
-        incomingMessage.member?.send(`Your code has already been generated ans sent to you. Please check again!`);
+        incomingMessage.member?.send(`Your code has already been generated and sent to you. Please check again!`);
     } else {
-        const availableRole = incomingMessage.guild?.roles.cache.find(role => role.name === userTypedRole);
-        console.log(availableRole);
+        const availableRole = incomingMessage.guild?.roles.cache.find(role => role.id === userTypedRole.substr(3,18));
         if (availableRole) {
+            const channel = incomingMessage.guild.channels.cache.find(
+                (ch: any) => ch.id === CONSTANTS.CODE_CHANNEL_ID
+            ) as TextChannel;
+            if (!channel) return;
+
             const read = await userRoleCol.insertOne(
                 {
                     id: incomingMessage.author.id,
                     code: code,
-                    role: availableRole.name,
+                    role: availableRole.id,
                 });
-            incomingMessage.member?.send(`Your code is - ${code}`);
+
+            const msgEmbed = new MessageEmbed()
+            .setColor("BLUE")
+            .setTitle(`New Code`)
+            .setDescription(`Hey <@${incomingMessage.author.id}>.\nYour code to get <@&${availableRole.id}> role is - ${code}\nKindly type the code in this channel itself to\n to get the role.`)
+            .setTimestamp()
+            .setFooter(CONSTANTS.FOOTER);
+            
+            channel.send(
+                { embeds: [msgEmbed] }
+            )
         } else {
             incomingMessage.member?.send(`You entered invalid role type.`);
         }
