84abe3e9e2baead1c0637127388e9498f818c2f6
Aniket Agarwal <aniketagarwal57@gmail.com>
2021-12-08 23:56:20 +0530

    chore:  add findoneandupdate code

--- index.ts
+++ index.ts
@@ -35,12 +35,19 @@ const createServer = async () => {
         }
       }
       if (message.channelId === CONSTANTS.CODE_CHANNEL_ID) {
-        const newUser = await userRoleCol.findOne({id: message.author.id});
+        const newUser = await userRoleCol.findOne({code: message.content});
         if (newUser) {
-            if (newUser.code == message.content) {
+          let userIdInArray = false;
+          for(let i=0;i<newUser.userIds.length;i++) {
+            if(newUser.userIds[i] === message.author.id) {
+              userIdInArray = true;
+            }
+          }
+            if (!userIdInArray) {
                 const roleToAssign: string = newUser.role;
                 if (roleToAssign) {
-                    const role = message.guild?.roles.cache.find(role => role.name === roleToAssign);
+                  userRoleCol.findOneAndUpdate({code: message.content},{$set: {userIds: [...newUser.userIds, message.author.id]}})
+                    const role = message.guild?.roles.cache.find(role => role.id === roleToAssign);
                     message.guild?.members.cache.get(message.author.id).roles.add(role);
                     const messages = await message.channel.messages.fetch({limit: 3})
 
