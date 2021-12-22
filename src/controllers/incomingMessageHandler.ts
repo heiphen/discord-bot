0ae25bfd8963852ec3a58f2fff088806d5b8d7f7
harshgoel05 <harshgoel05@gmail.com>
2021-12-22 13:09:40 +0530

    fix: code format

--- src/controllers/incomingMessageHandler.ts
+++ src/controllers/incomingMessageHandler.ts
@@ -6,32 +6,35 @@ import { handleGetCountAll } from "../helper/getCountAll";
 import { handleGetMemberCount } from "../helper/memberCount";
 import { COMMANDS } from "../utils/constants";
 
-export async function handleIncomingChannelCommand(incomingMessage: Message, userRoleCol: Collection) {
-    try {
-      const messageCommand = incomingMessage.content.split(/\s+/)[1];
-  
-      switch (messageCommand) {
-        case COMMANDS.membercount: {
-          handleGetMemberCount(incomingMessage);
-          break;
-        }
-        case COMMANDS.createcode: {
-          handleCreateCode(incomingMessage, userRoleCol);
-          break;
-        }
-        case COMMANDS.getCount: {
-          handleGetCount(incomingMessage, userRoleCol);
-          break;
-        }
-        case COMMANDS.getCountAll: {
-          handleGetCountAll(incomingMessage, userRoleCol);
-          break;
-        }
-        default:
-          console.log(`Invalid Command    ${incomingMessage.content}`);
-          break;
+export async function handleIncomingChannelCommand(
+  incomingMessage: Message,
+  userRoleCol: Collection
+) {
+  try {
+    const messageCommand = incomingMessage.content.split(/\s+/)[1];
+
+    switch (messageCommand) {
+      case COMMANDS.membercount: {
+        handleGetMemberCount(incomingMessage);
+        break;
       }
-    } catch (err) {
-        console.log(`${err}    ${incomingMessage.content}`);
+      case COMMANDS.createcode: {
+        handleCreateCode(incomingMessage, userRoleCol);
+        break;
+      }
+      case COMMANDS.getCount: {
+        handleGetCount(incomingMessage, userRoleCol);
+        break;
+      }
+      case COMMANDS.getCountAll: {
+        handleGetCountAll(incomingMessage, userRoleCol);
+        break;
+      }
+      default:
+        console.log(`Invalid Command    ${incomingMessage.content}`);
+        break;
     }
-  };
+  } catch (err) {
+    console.log(`${err}    ${incomingMessage.content}`);
+  }
+}
