c9d860f95bc964d57b96cdcedab5335e1d6d0d81
harshgoel05 <harshgoel05@gmail.com>
2021-12-22 13:05:13 +0530

    chore: format code

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 import { Intents, Client, Message } from "discord.js";
 import { handleIncomingChannelCommand } from "./src/controllers/incomingMessageHandler";
 import { handleMemberJoin } from "./src/helper/memberLogs";
@@ -10,68 +10,76 @@ const intents = new Intents(32767);
 const client = new Client({ intents });
 
 const createServer = async () => {
-
   const dbClient = await getDbClient();
   const db = dbClient.db("HeiphenBot");
   const userRoleCol = db.collection("UserRole");
-  
+
   client.on("ready", () => console.log("Bot is online!"));
-  
-  
+
   client.on("messageCreate", async (message: Message) => {
-  
     if (!message.author.bot) {
       if (message.content.split(/\s+/)[0] == COMMANDS.prefix) {
         switch (message.channel.type) {
-  
           case "GUILD_TEXT": {
             handleIncomingChannelCommand(message, userRoleCol);
             break;
           }
-  
+
           default: {
             console.log(`error     ChannelNotSupported`);
           }
         }
       }
       if (message.channelId === CONSTANTS.CODE_CHANNEL_ID) {
-        const newUser = await userRoleCol.findOne({code: message.content});
+        const newUser = await userRoleCol.findOne({ code: message.content });
         if (newUser) {
           let userIdInArray = false;
-          for(let i=0;i<newUser.userIds.length;i++) {
-            if(newUser.userIds[i] === message.author.id) {
+          for (let i = 0; i < newUser.userIds.length; i++) {
+            if (newUser.userIds[i] === message.author.id) {
               userIdInArray = true;
             }
           }
-            if (!userIdInArray) {
-                const roleToAssign: string = newUser.role;
-                if (roleToAssign) {
-                  userRoleCol.findOneAndUpdate({code: message.content},{$set: {userIds: [...newUser.userIds, message.author.id]}})
-                    const role = message.guild?.roles.cache.find(role => role.id === roleToAssign);
-                    message.guild?.members.cache.get(message.author.id).roles.add(role);
-                    const messages = await message.channel.messages.fetch({limit: 3})
+          if (!userIdInArray) {
+            const roleToAssign: string = newUser.role;
+            if (roleToAssign) {
+              userRoleCol.findOneAndUpdate(
+                { code: message.content },
+                { $set: { userIds: [...newUser.userIds, message.author.id] } }
+              );
+              const role = message.guild?.roles.cache.find(
+                (role) => role.id === roleToAssign
+              );
+              if (!role) return;
+              // @ts-ignore
+              message?.guild?.members?.cache
+                .get(message?.author?.id)
+                .roles.add(role);
+              const messages = await message.channel.messages.fetch({
+                limit: 3,
+              });
 
-                    if (message.channel.type === "GUILD_TEXT") message.channel.bulkDelete(messages);
-                    
-                    message.channel.send(`<@${message.author.id}> You have been assigned <@&${role?.id}> role.`);
-                }
-            } else {
-                message.member?.send(`Your code is incorrect.`);
+              if (message.channel.type === "GUILD_TEXT")
+                message.channel.bulkDelete(messages);
+
+              message.channel.send(
+                `<@${message.author.id}> You have been assigned <@&${role?.id}> role.`
+              );
             }
+          } else {
+            message.member?.send(`Your code is incorrect.`);
+          }
         }
       }
     }
   });
-  
+
   client.on("guildMemberAdd", (member) => {
     console.log(member.guild.invites.cache);
     console.log(member.guild.roles.cache);
-      handleMemberJoin(member);
-    });
+    handleMemberJoin(member);
+  });
 };
 
 createServer();
 
-
 client.login(process.env.TOKEN);
-
